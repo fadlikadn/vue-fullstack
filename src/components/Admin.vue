3b6f7bcc47e996d75d9907dc405685edcf53fa16
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-11-28 14:54:16 +0700

    Listing current orders

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,26 +1,57 @@
 <template>
-    <div class="row">
-        <div class="col-sm-12 col-md-6">
-            <!-- new pizza component -->
+    <div>
+        <div class="row">
+            <div class="col-sm-12 col-md-6">
+                <!-- new pizza component -->
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <h3>Menu: </h3>
+                <table class="table table-hover">
+                    <thead class="thead-default">
+                        <tr>
+                            <th>Item</th>
+                            <th>Remove from menu</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Margaretha</td>
+                            <td>
+                                <button class="btn btn-outline-danger btn-sm">x</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
-        <div class="col-sm-12 col-md-6">
-            <h3>Menu: </h3>
-            <table class="table table-hover">
-                <thead class="thead-default">
-                    <tr>
-                        <th>Item</th>
-                        <th>Remove from menu</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>Margaretha</td>
-                        <td>
+
+        <div class="row">
+            <div class="col-sm-12">
+                <h3>Current Orders:</h3>
+                <table class="table table-sm">
+                    <thead class="thead-default">
+                        <tr>
+                            <th>Item</th>
+                            <th>Size</th>
+                            <th>Quantity</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <div class="order-number">
+                            <strong><em>Order Number: 1</em></strong>
                             <button class="btn btn-outline-danger btn-sm">x</button>
-                        </td>
-                    </tr>
-                </tbody>
-            </table>
+                        </div>
+                        <tr>
+                            <td>Margaretha</td>
+                            <td>9"</td>
+                            <td>1</td>
+                            <td>6.95</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </div>
     </div>
 </template>
