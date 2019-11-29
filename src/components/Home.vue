b29d48c4b92e457fcb239cc8aae2aa5ffa8effb6
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-11-29 14:09:05 +0700

    Router navigation methods

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,11 +5,21 @@
                 <h1>Welcome to Pizza Planet</h1>
                 <h2>Feeling Hungry?</h2>
 
-                <button class="btn btn-success">Let's order!</button>
+                <button class="btn btn-success" @click="goToMenu()">Let's order!</button>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    methods: {
+        goToMenu() {
+            this.$router.push({ name: 'menuLink' });
+        },
+    }
+}
+</script>
 
 <style>
     #home {
