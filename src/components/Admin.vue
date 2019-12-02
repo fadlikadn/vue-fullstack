f1b9e119e97b5379458112479ce9ad180952cc92
Fadlika Dita Nurjanto <fadlikadn@gmail.com>
2019-12-02 08:32:41 +0700

    FIrebase sync : Hiding the admin from unauthorised users

--- src/components/Admin.vue
+++ src/components/Admin.vue
@@ -1,58 +1,60 @@
 <template>
     <div>
-        <div class="row">
-            <div class="col-sm-12 col-md-6">
-                <pp-new-pizza></pp-new-pizza>
+        <section v-if="currentUser">
+            <div class="row">
+                <div class="col-sm-12 col-md-6">
+                    <pp-new-pizza></pp-new-pizza>
+                </div>
+                <div class="col-sm-12 col-md-6">
+                    <h3>Menu: </h3>
+                    <table class="table table-hover">
+                        <thead class="thead-default">
+                            <tr>
+                                <th>Item</th>
+                                <th>Remove from menu</th>
+                            </tr>
+                        </thead>
+                        <tbody v-for="item in getMenuItems" :key="item['.key']">
+                            <tr>
+                                <td>{{ item.name }}</td>
+                                <td>
+                                    <button class="btn btn-outline-danger btn-sm" @click="removeMenuItem(item['.key'])">x</button>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-            <div class="col-sm-12 col-md-6">
-                <h3>Menu: </h3>
-                <table class="table table-hover">
-                    <thead class="thead-default">
-                        <tr>
-                            <th>Item</th>
-                            <th>Remove from menu</th>
-                        </tr>
-                    </thead>
-                    <tbody v-for="item in getMenuItems" :key="item['.key']">
-                        <tr>
-                            <td>{{ item.name }}</td>
-                            <td>
-                                <button class="btn btn-outline-danger btn-sm" @click="removeMenuItem(item['.key'])">x</button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
 
-        <div class="row">
-            <div class="col-sm-12">
-                <h3>Current Orders: {{ numberOfOrders }} </h3>
-                <table class="table table-sm" v-for="(orders, index) in getOrders" :key="orders['.key']">
-                    <thead class="thead-default">
-                        <tr>
-                            <th>Item</th>
-                            <th>Size</th>
-                            <th>Quantity</th>
-                            <th>Price</th>
-                        </tr>
-                    </thead>
+            <div class="row">
+                <div class="col-sm-12">
+                    <h3>Current Orders: {{ numberOfOrders }} </h3>
+                    <table class="table table-sm" v-for="(orders, index) in getOrders" :key="orders['.key']">
+                        <thead class="thead-default">
+                            <tr>
+                                <th>Item</th>
+                                <th>Size</th>
+                                <th>Quantity</th>
+                                <th>Price</th>
+                            </tr>
+                        </thead>
 
-                    <tbody>
-                        <div class="order-number">
-                            <strong><em>Order Number: {{ index +1 }}</em></strong>
-                            <button class="btn btn-outline-danger btn-sm" @click="removeOrderItem(orders['.key'])">x</button>
-                        </div>
-                        <tr v-for="ordersItem in orders">
-                            <td>{{ ordersItem.name }}</td>
-                            <td>{{ ordersItem.size }}</td>
-                            <td>{{ ordersItem.quantity }}</td>
-                            <td>{{ ordersItem.price }}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                        <tbody>
+                            <div class="order-number">
+                                <strong><em>Order Number: {{ index +1 }}</em></strong>
+                                <button class="btn btn-outline-danger btn-sm" @click="removeOrderItem(orders['.key'])">x</button>
+                            </div>
+                            <tr v-for="ordersItem in orders">
+                                <td>{{ ordersItem.name }}</td>
+                                <td>{{ ordersItem.size }}</td>
+                                <td>{{ ordersItem.quantity }}</td>
+                                <td>{{ ordersItem.price }}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
+        </section>        
 
         <hr>
         <div class="row">
@@ -86,6 +88,7 @@ export default {
             'numberOfOrders',
             'getMenuItems',
             'getOrders',
+            'currentUser',
         ]),
     },
     methods: {
